eae4d70a7da04b4b391eed2e64684aaa1c462cef
ehima <ehimareatga063@gmail.com>
2025-10-16 16:32:25 +0100

    Initial changes to my premierinsight projewject e.g adding api data and define the variables etc..

---
+++ Data/FplService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json; 
+using PremierInsight.Data.Models;
+
+namespace PremierInsight.Data
+{
+    public class FplService
+    {
+        private readonly HttpClient _httpClient;
+        private const string FplApiUrl = "https://fantasy.premierleague.com/api/bootstrap-static/";
+
+        public FplService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<FplRoot?> GetFplDataAsync()
+        {
+            var response = await _httpClient.GetStringAsync(FplApiUrl);
+            var data = JsonConvert.DeserializeObject<FplRoot>(response);
+            return data;
+        }
+    }
+}
---
+++ Data/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PremierInsight.Data.Models
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string second_name { get; set; }
+        public string web_name { get; set; }
+        public int team { get; set; }
+        public float now_cost { get; set; }
+        public int total_points { get; set; }
+    }
+
+    public class FplRoot
+    {
+        public List<Player> elements { get; set; }
+    }
+}
