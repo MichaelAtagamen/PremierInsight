fec88eec325ae2a8448d549f4bd2871a33923d68
ehima <ehimareatga063@gmail.com>
2025-10-18 19:45:21 +0100

    I cerated this TeamStanding to give a model of the name postion and what team is placed in their position based on their points the most indicates the best team so far.

---
+++ Data/Models/TeamStanding.cs
@@ -0,0 +1,13 @@
+﻿namespace PremierInsight.Data.Models
+{
+    // Just the usual getters and setters 
+    public class TeamStanding
+    {
+        public int Position { get; set; }
+        public string TeamName { get; set; } = "";
+        public int PlayedGames { get; set; }
+        public int Points { get; set; }
+        public int GoalsFor { get; set; }
+        public int GoalsAgainst { get; set; }
+    }
+}
